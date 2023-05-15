c7789755f4c738d226ece31735306ed2c1759074
j.durdiev <durdiev@ttronics.ru>
2023-05-15 09:26:14 +0500

    Упр1

--- Chapter_5.cpp
+++ Chapter_5.cpp
@@ -1,4 +1,24 @@
-// Решение упражнений 5 Главы
 
-// Упражнение 1
-char* ch = 'a';
+#include <iostream>
+#include <cstdlib> // для system
+
+using namespace std;
+
+// Решение упражнений 5 Главы
+    // Упражнение 1
+        //Указатель на символ
+char ch = 'A';
+char *c = &ch;
+        //массив из 10 целых
+int m[10] = {1, 2, 3, 4};
+        //ссылка на массив из 10 целых
+
+int &mm = m[0];
+
+//m = 5;
+
+int main()
+{
+    printf("%i",mm);
+    return 0;
+}
